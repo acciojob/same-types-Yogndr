8946fc9382e5ea60ddbaf69dc51048e56f9dfa53
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-17 01:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function isSameType(value1, value2) {
 	if((typeof(value1)===typeof(value2)) ||(isNaN(value1) && isNaN(value2))){
 		return true;
 	}
-	else if(typeof(value1)!==typeof(value))
+	else if(typeof(value1)!==typeof(value2))
 	{
 		return false;
 	}
